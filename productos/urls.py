0ed299202aec75f6c9151ba2a53959217eeb5631
heer764 <se.andrades@duocuc.cl>
2024-01-26 23:25:10 -0300

    ahora está listo el backend de productos

--- productos/urls.py
+++ productos/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path('crud_productos', views.crud_productos, name='crud_productos'),
     path('productos_add', views.productos_ag, name='productos_add'),
+    path('productos_edit/<str:pk>', views.productos_edit, name='productos_edit'),
+    path('productosUpdate', views.productosUpdate, name='productosUpdate'),
+    path('productos_del/<str:pk>', views.productos_del, name='productos_del')
 ]
--- productos/views.py
+++ productos/views.py
@@ -28,3 +28,65 @@ def productos_ag(request):
         form = ProductoForm()
         context = {'form':form}
         return render (request, 'productos/productos_add.html', context)
+    
+def productos_edit(request, pk):
+    if pk != "":
+        producto=Producto.objects.get(id_producto=pk)
+        context={'producto':producto}
+        if producto:
+            return render(request, 'productos/productos_edit.html', context)
+        else:
+            context={'mensaje':"Lo lamentamos, no existe tal producto."}
+            return render(request, 'productos/productos_list.html', context)
+    else:
+        context={}
+        return render(request, 'productos/productos_list.html', context)
+    
+def productosUpdate(request):
+    if request.method == "POST":
+        modelo=request.POST["modelo"]
+        marca=request.POST["marca"]
+        anno_fab=request.POST["anno_fab"]
+        precio=request.POST["precio"]
+        stock=request.POST["stock"]
+        estado=request.POST["estado"]
+        categoria=request.POST["categoria"]
+        desc=request.POST["desc"]
+        producto = Producto()
+        producto.modelo=modelo
+        producto.marca=marca
+        producto.anno_fab=anno_fab
+        producto.precio=precio
+        producto.stock=stock
+        producto.estado=estado
+        producto.categoria=categoria
+        producto.desc=desc
+        producto.save()
+        context={'mensaje':"Perfecto! Datos actualizados exitosamente", 'producto':producto}
+        return render(request, 'productos/productos_list.html', context)
+    else:
+        productos = Producto.objects.all()
+        context ={'productos':productos}
+        return render(request, 'productos/productos_list.html', context)
+    
+def productos_del(request, pk):
+    mensajes=[]
+    errores=[]
+    productos = Producto.objects.all()
+    try:
+        producto = Producto.objects.get(id_producto=pk)
+        context={}
+        if producto:
+            producto.delete()
+            mensajes.append("Perfecto! Datos eliminados")
+            context = {'productos':productos, 'mensajes':mensajes, 'errores':errores}
+            return render(request, 'productos/productos_list.html', context)
+        else:
+            context={}
+            return render(request, 'productos/productos_list.html', context)
+    except:
+        print("Lo sentimos! No existe tal producto")
+        productos=Producto.objects.all()
+        mensaje="Lo sentimos! No existe tal producto"
+        context={'mensaje':mensaje, 'productos':productos}
+        return render (request, 'productos/productos_list.html', context)
